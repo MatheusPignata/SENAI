db3c31b61b00575350d729fd5bdeebff663feccb
MatheusPignata <pignatamatheus@gmail.com>
2021-09-28 08:04:44 -0300

    Att

--- 2 DES/PWFE/Aula 14/albuns/index.js
+++ 2 DES/PWFE/Aula 14/albuns/index.js
@@ -1,11 +1,62 @@
 const url = "https://jsonplaceholder.typicode.com/albums?userId=" + localStorage.getItem("id");
+var albuns = document.querySelector(".albuns");
 var username = document.querySelector("#username");
 username.innerHTML = localStorage.getItem("name");
 
 fetch(url).then(resp => {
     return resp.json();
 }).then(data => {
-    console.log(data);
+    data.forEach(album => {
+       generateHTML(album.id, album.title);
+    })
 }).catch(error => {
     console.log(error);
 })
+
+function generateHTML(id, title) {
+    let album = document.querySelector(".album").cloneNode(true);
+    album.classList.remove("model");
+    album.querySelector("h4").innerHTML = title;
+    album.addEventListener("click", () => {
+        document.querySelector("body").style.overflow = "hidden";
+        document.querySelector(".modal").classList.remove("model");
+        getPhotos(id);
+    });
+    albuns.appendChild(album);
+}
+
+function getPhotos(id) {
+    let urlPhoto = "https://jsonplaceholder.typicode.com/photos?albumId=" + id;
+
+    fetch(urlPhoto).then(resp => {
+        return resp.json()
+    }).then(data => {
+        let lista = document.querySelector(".lista");
+        lista.innerHTML = "";
+        data.forEach(foto => {
+            let img = document.createElement("img");
+            img.style.width = "20%";
+            img.src = foto.url;
+            lista.appendChild(img);
+        })
+    }).catch(error => {
+        console.log(error);
+    })
+}
+
+function closeModal() {
+    document.querySelector("body").style.overflow = "visible";
+    document.querySelector(".modal").classList.add("model");
+}
+
+function toAlbuns(){
+    window.location.href = "/albuns";
+}
+
+function toPosts(){
+    window.location.href = "/posts";
+}
+
+function toHome(){
+    window.location.href = "/home";
+}
--- 2 DES/PWFE/Aula 14/posts/index.js
+++ 2 DES/PWFE/Aula 14/posts/index.js
@@ -1,22 +1,25 @@
 var username = document.querySelector("#username");
-const url = "https://jsonplaceholder.typicode.com/todos?userId=" + localStorage.getItem("id");
+const url = "https://jsonplaceholder.typicode.com/posts?userId=" + localStorage.getItem("id");
 const list = document.querySelector(".list");
-
 username.innerHTML = localStorage.getItem("name");
 
 fetch(url).then(resp => {
     return resp.json();
 }).then(data => {
-    data.forEach(todo =>{
+    data.forEach(post =>{
         let item = document.querySelector(".item").cloneNode(true);
         item.classList.remove("model");
-        item.querySelector("p").innerHTML = todo.title;
-        if(todo.completed) item.querySelector("input").setAttribute("checked", true);
+        item.querySelector("h3").innerHTML = post.title;
+        item.querySelector("p").innerHTML = post.body;
         list.appendChild(item);
-    })
+    })  
 }).catch(error => {
     console.log(error);
 })
+
+function excludePost(e){
+    e.parentNode.remove();
+}
 
 function toAlbuns(){
     window.location.href = "/albuns";
